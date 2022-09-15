1646475912e2d890cd3c904dfaf1094c91bb2dca
solarstar101 <45614244+solarstar101@users.noreply.github.com>
2022-09-14 21:24:45 -0500

    created base react application and setup redux and rtk also added logic for infinite scrolling into the application

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useSingleRecentDrawQuery } from '../src/services/draws'
+import { usePastDrawsStartingPointQuery } from "../src/services/draws";
+
+function App() {
+  const [drawQuery, setDrawQuery] = useState({
+    drawid: '',
+    number: 0,
+    sort: ''
+  })
+  const [completeData, setcompleteData]: any = useState([])
+  const [completeDataLoading, setcompleteDataLoading]: any = useState(false)
+
+  const { data: singleRecentDraw, error: singleRecentDrawError, isLoading: singleRecentDrawLoading } = useSingleRecentDrawQuery()
+  const { data: completedrawdata, error: completedDrawError, isLoading: completedDrawLoading } = usePastDrawsStartingPointQuery(drawQuery,
+    {
+      skip: drawQuery.drawid === '' && drawQuery.number === 0 && drawQuery.sort === '',
+      refetchOnMountOrArgChange: true
+    });
+
+  useEffect(() => {
+    if (!singleRecentDrawLoading && !singleRecentDrawError) {
+      setcompleteDataLoading(true)
+      setDrawQuery({ drawid: singleRecentDraw[0].gameNumber, number: 20, sort: 'dsc' })
+    }
+  }, [singleRecentDrawLoading])
+
+  useEffect(() => {
+    if (!completedDrawLoading && completedrawdata) {
+      setcompleteData([...completeData, ...completedrawdata])
+      setcompleteDataLoading(false)
+
+    }
+  }, [completedrawdata])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+      setDrawQuery({ drawid: completedrawdata[completedrawdata.length - 1].gameNumber, number: 20, sort: 'dsc' })
+      setcompleteDataLoading(true)
+
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [completedrawdata]);
+
+
+  return (
+    <div className="App">
+      {!completedDrawLoading && completeData.length >= 1 && completeData.map((obj: any) =>
+        <h1>
+          {obj.gameNumber}
+        </h1>
+      )}
+      {completeDataLoading && <h2>LOADING NEW DATA</h2>}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/store.ts
@@ -0,0 +1,24 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { drawApiRecent,drawApiDrawID } from '../services/draws'
+
+export const store = configureStore({
+  reducer: {
+    [drawApiRecent.reducerPath]: drawApiRecent.reducer,
+    [drawApiDrawID.reducerPath]: drawApiDrawID.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(drawApiRecent.middleware),
+});
+
+// export type AppDispatch = typeof store.dispatch;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppThunk<ReturnType = void> = ThunkAction<
+//   ReturnType,
+//   RootState,
+//   unknown,
+//   Action<string>
+// >;
+
+
+setupListeners(store.dispatch)
